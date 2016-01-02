08d224c25bc7d27705a5ef74951284f747068d00
naitianliu <naitianliu@gmail.com>
2016-01-01 18:19:06 -0800

    Initial Commit

---
+++ Sirius/Sirius/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Sirius
+//
+//  Created by Liu, Naitian on 1/1/16.
+//  Copyright © 2016 naitianliu. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Sirius/Sirius/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Sirius
+//
+//  Created by Liu, Naitian on 1/1/16.
+//  Copyright © 2016 naitianliu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
